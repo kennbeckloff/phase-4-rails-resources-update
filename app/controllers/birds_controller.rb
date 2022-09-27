c9a8e65ed90c99dbb7a322e9a7b607c2ff479a57
kennbeckloff <kennbeckloff688@gmail.com>
2022-09-27 19:51:36 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -27,5 +27,14 @@ class BirdsController < ApplicationController
   def bird_params
     params.permit(:name, :species)
   end
-
+  # PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
 end
---
+++ db/migrate/20220927164344_add_likes_to_bird.rb
@@ -0,0 +1,4 @@
+class AddLikesToBird < ActiveRecord::Migration[6.1]
+  def change
+    add_column :birds, :likes, :integer, null: false, default: 0  end
+end
